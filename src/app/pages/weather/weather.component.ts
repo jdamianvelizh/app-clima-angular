2dcf9c469f45c7c87202c5e1d24fc7c1139c16e0
DamianVH1 <josephveliz99@hotmail.com>
2023-07-08 22:34:49 -0600

    Creacion de estilos de la card y modificacion de weather comp

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { WeatherData } from '@app/shared/weather.interface';
 import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WeatherComponent {
 
+  @Input() public weather!: WeatherData;
 
+  public BASE_URL = 'https://openweathermap.org/img/wn'
 }
